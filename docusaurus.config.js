082ae5e023df3508a548afa4fe6c6f15c735f8c3
zhengwe <935006979@qq.com>
2023-07-27 21:08:09 +0800

    update index

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,8 +6,8 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'zheng wei',
-  tagline: 'Dinosaurs are cool',
+  title: 'zhengwei',
+  tagline: 'You are the only thing I want to know',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
@@ -65,7 +65,7 @@ const config = {
       // Replace with your project's social card
       image: 'img/docusaurus-social-card.jpg',
       navbar: {
-        title: 'My Site',
+        title: 'zheng wei',
         logo: {
           alt: 'My Site Logo',
           src: 'img/logo.svg',
@@ -74,12 +74,13 @@ const config = {
           {
             type: 'docSidebar',
             sidebarId: 'tutorialSidebar',
-            position: 'right',
+            position: 'left',
             label: 'Doc',
           },
-          {to: '/blog', label: 'Blog', position: 'right'},
+          {to: '/blog', label: 'Blog', position: 'left'},
+          {to: '/about', label: 'About', position: 'left'},
           {
-            href: 'https://github.com/facebook/docusaurus',
+            href: 'https://github.com/zhengwe/zhengwe.com',
             label: 'GitHub',
             position: 'right',
           },
@@ -128,7 +129,7 @@ const config = {
             ],
           },
         ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+        copyright: `Copyright © ${new Date().getFullYear()} Innocence`,
       },
       prism: {
         theme: lightCodeTheme,
